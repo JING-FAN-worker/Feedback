aaadccb599214870169f78ebe8d0991c645ce0db
JING-FAN-worker <127564066+JING-FAN-worker@users.noreply.github.com>
2024-02-27 10:21:33 +0200

    fi

--- feedback.js
+++ feedback.js
@@ -8,13 +8,21 @@ if (document.readyState !== "loading"){
     })
 }
 
+let noteCounter = 0;
+
 function initializeCode() {
     const submitButton = document.getElementById("submit");
+    const removeButton = document.getElementById("remove");
     submitButton.addEventListener("click", function(){
         const notes = document.getElementById("notes")
         let newParagraph = document.createElement("p");
-        newParagraph.innerText = document.getElementById("comment").value
+        newParagraph.innerText = ++noteCounter + ". " + document.getElementById("comment").value
         notes.appendChild(newParagraph);
+})
+    removeButton.addEventListener("click", function(){
+        const notes = document.getElementById("notes")
+        notes.removeChild(notes.lastChild);
+        --noteCounter;
 })
 
 }
